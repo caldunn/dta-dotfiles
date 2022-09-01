7494083d55342a5282b36cbc8a9a8a8b847794bf
Caleb <caldunn@outlook.com>
2022-09-01 11:11:08 +0800

    .profile now checks if it has already been loaded. Resolves xorg & .zshrc reload

--- xrandr.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-xrandr --output HDMI-1 --primary --mode 1920x1080 --pos 2560x557 --rotate normal --output HDMI-2 --off --output DP-1 --off --output HDMI-3 --off --output DP-2 --mode 2560x1440 --pos 0x0 --rotate normal --output HDMI-4 --off
