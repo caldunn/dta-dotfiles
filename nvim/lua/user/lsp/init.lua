472033fc8762319569b4b63a1213316bcd5164d3
Caleb <caldunn@outlook.com>
2022-10-25 22:48:25 +0800

    colours and lsp reconfig

--- nvim/lua/user/lsp/init.lua
+++ nvim/lua/user/lsp/init.lua
@@ -1,13 +1,8 @@
-local ok, mason = pcall(require, "mason")
-if not ok then 
-  return 
-end 
-local ok, _ = pcall(require, "lspconfig")
-if not ok then 
-  print("LSP Config is not loading")
-  return 
+local status_ok, _ = pcall(require, "lspconfig")
+if not status_ok then
+  return
 end
 
-
-require('user/lsp/lsp-installer')
-mason.setup()
+require "user.lsp.mason"
+require("user.lsp.handlers").setup()
+require "user.lsp.null-ls"
